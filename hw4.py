b8d863f5aee460b9ca7adce7e75242e59e74b990
Stacie <skuamoo@g.harvard.edu>
2015-10-25 11:21:16 -0400

    made changes to separate by acts and scenes

--- hw4.py
+++ hw4.py
@@ -6,66 +6,74 @@ import re
 import nltk
 from nltk.corpus import stopwords
 import string
-
+from collections import defaultdict
+#Open MIT Shakespeare page and extract all links
 html = urlopen("http://shakespeare.mit.edu/")
-bsObj = BeautifulSoup(html)
+page = BeautifulSoup(html)
+play_links = page.findAll("a",href=re.compile("(.html)"))
 
-hyperlinks = bsObj.findAll("a",href=re.compile("(.html)"))
+data_scenes = []
+data_acts = []
 
-data = []
-
-for links in hyperlinks:
-    if 'href' in links.attrs:
-        if(links.attrs['href'] == "news.html"):
+for link in play_links:
+    if 'href' in link.attrs:
+        #skip news link
+        if(link.attrs['href'] == "news.html"):
             continue
         
-        path = str(links.attrs['href'])
-        
+        path = str(link.attrs['href'])        
         end = path.find('/')
         directory = path[0:end]   
-        html1 =  urlopen("http://shakespeare.mit.edu/" + links.attrs['href'] )
-        bsObj1 = BeautifulSoup(html1)
-        
-        if(directory != "Poetry"):
-            hl2 = bsObj1.findAll("a",href=re.compile("(.html)"))
+        html_plays =  urlopen("http://shakespeare.mit.edu/" + link.attrs['href'] )
+        plays = BeautifulSoup(html_plays)
+        #title = plays.find('td').contents[0].strip()
+        if(directory == "Poetry"):
+            #Skip poetry due to inherent stylistic differences
+            continue
         else:
-            hl2 = bsObj1.findAll("a",href=re.compile("(.html)"))
-            #if(not hl2):
-                #print("Reading file: http://shakespeare.mit.edu/" + str(links.attrs['href']))
-                #content = urlopen("http://shakespeare.mit.edu/" + str(links.attrs['href']))
-                #bsObj2 = BeautifulSoup(content)
-                #data = data + str(bsObj2.get_text())
-                
-        for links2 in hl2:
+            act_links = plays.findAll("a",href=re.compile("(.html)"))
+
+        act_text = defaultdict(lambda:defaultdict(list))
+        act_lines = defaultdict(lambda:defaultdict(list))
+        for link2 in act_links:
             scene_lines = []
-            if(links2.attrs['href'] == "full.html"):
+            #Skip link to full play
+            if(link2.attrs['href'] == "full.html"):
                 continue
-            if 'href' in links2.attrs and directory != "Poetry":
-                link_href = str(links2.attrs['href'])
+            if 'href' in link2.attrs:
+                link_href = str(link2.attrs['href'])
                 #print("Reading file: http://shakespeare.mit.edu/" + directory + "/" + link_href)
-                html2 = urlopen("http://shakespeare.mit.edu/" + directory + "/" + link_href)
-                bsObj2 = BeautifulSoup(html2)
-                hl3 = bsObj2.findAll('a')
-                title = bsObj2.find('td').contents[0].strip()
+                html_acts = urlopen("http://shakespeare.mit.edu/" + directory + "/" + link_href)
+                acts = BeautifulSoup(html_acts)
+                scenes = acts.findAll('a')
+                title = acts.find('td').contents[0].strip()
                 act_start = link_href.find(".", link_href.find(directory + "."))+1
                 act_end = link_href.find(".", act_start)
                 act = link_href[act_start:act_end]
                 scene = link_href[act_end+1:link_href.find(".html")]                               
-                #page = str(bsObj2.contents[1])
-                #act = page[page.find("Act")+4:page.find(",", page.find("Act")+4)].strip()
-                #scene = page[page.find("Scene")+6:page.find("<", page.find("Scene")+6)].strip()
-                for links3 in hl3:
-                    if 'name' in links3.attrs:
-                        if links3.attrs['name'].isnumeric():
-                            scene_lines.append(links3.contents[0].strip())
+                for link3 in scenes:
+                    if 'name' in link3.attrs:
+                        if link3.attrs['name'].isnumeric():
+                            scene_lines.append(link3.contents[0].strip())
                 text = ' '.join(scene_lines)
                 tokens = nltk.word_tokenize(text)
-                tokens_cleaned = [word for word in tokens if word.lower() not in stopwords.words('english') and len(word) > 2]
-                freq = dict(nltk.FreqDist(tokens_cleaned))
-                scene_obj = {'title':title, 'act':act, 'scene':scene, 'lines':scene_lines, 'text':text, 'freq':freq}
-            else:
-                continue
-            data.append(scene_obj)
-f = open('shakespeare.txt', 'w')
-f.writelines(str(data))
+                #tokens_cleaned = [word for word in tokens if word.lower() not in stopwords.words('english') and len(word) > 2]
+                #freq = dict(nltk.FreqDist(tokens))
+                scene_obj = {'title':title, 'act':act, 'scene':scene, 'lines':scene_lines, 'text':text}
+                data_scenes.append(scene_obj)
+                act_lines[title][act] += scene_lines
+                act_text[title][act].append(text)
+
+        for key in sorted(act_lines.keys()):
+            for act_num in sorted(act_lines[key].keys()):
+                act_obj = {'title':key, 'act':act_num, 'lines':act_lines[key][act_num], 'text':' '.join(act_text[key][act_num])}
+                data_acts.append(act_obj)
+
+#Write data by scene to file
+f = open('shakespeare_scenes.txt', 'w')
+f.writelines(str(data_scenes))
+f.close()
+#Write data by act to file
+f = open('shakespeare_acts.txt', 'w')
+f.writelines(str(data_acts))
 f.close()
